9ea014f116f0fcb3d78e1c3148f4f7ca9c1373b2
Saker Lin <iamsaker.s001@gmail.com>
2020-02-06 10:15:46 +0800

    Add a fucntion: reverse.js

---
+++ reverse.js
@@ -0,0 +1,12 @@
+function reverseWords (str) {
+  strArr = str.split(' ')
+
+  strArrReverse = strArr.map(str => {
+    let newStr = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+      newStr += str[i]
+    }
+    return newStr
+  })
+  return strArrReverse.join(' ')
+}
